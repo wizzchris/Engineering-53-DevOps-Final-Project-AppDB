29d06e118c9f9e035f13fa0227ab2f29f392b0a3
jemurphyuk <60648948+jemurphyuk@users.noreply.github.com>
2020-03-20 09:45:58 +0000

    Update default_spec.rb

--- mongodb_cookbook/spec/unit/recipes/default_spec.rb
+++ mongodb_cookbook/spec/unit/recipes/default_spec.rb
@@ -7,7 +7,7 @@
 require 'spec_helper'
 require 'aws-sdk'
 
-describe 'node_cookbook::default' do
+describe 'mongodb_cookbook::default' do
   context 'When all attributes are default, on Ubuntu 16.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
@@ -20,7 +20,7 @@ describe 'node_cookbook::default' do
       it { should be_installed.with_version('3.6.17') }
     end
     it 'should create a mongod.conf template in /home/ubuntu/environment/mongod.conf' do
-      expect(chef_run).to create_template "/home/ubuntu/environment/mongod.conff"
+      expect(chef_run).to create_template "/home/ubuntu/environment/mongod.conf"
     end
 
   end
